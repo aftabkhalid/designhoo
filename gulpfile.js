fc997f395d28955215fe5c68603ca8fc08c7ef24
Aftab Khalid <iffi.d.s@gmail.com>
2022-06-13 00:56:14 +0500

    initialized the project

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
+'use strict';
 
-// gulpfile.js
-var gulp = require("gulp"),
-    sass = require("gulp-sass"),
-    postcss = require("gulp-postcss"),
-    autoprefixer = require("autoprefixer"),
-    cssnano = require("cssnano"),
-    sourcemaps = require("gulp-sourcemaps");
-    browserSync = require("browser-sync").create();
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const sourcemaps = require("gulp-sourcemaps");
+const browserSync = require("browser-sync").create();
 
 //Paths
 var paths = {
@@ -14,17 +14,6 @@ var paths = {
     src: "sass/theme.scss",
     dest: "css"
   },
-
-  bootstrap: {
-    src: "node_modules/bootstrap/scss/bootstrap.scss",
-    dest: "css"
-  },
-
-  // Easily add additional paths
-  // ,html: {
-  //  src: '...',
-  //  dest: '...'
-  // }
 };
 
 function style() {
@@ -35,45 +24,13 @@ function style() {
       .pipe(sourcemaps.init())
       .pipe(sass())
       .on("error", sass.logError)
-      .pipe(postcss([autoprefixer(), cssnano()]))
+      //.pipe(postcss([autoprefixer(), cssnano()]))
       .pipe(sourcemaps.write())
       .pipe(gulp.dest(paths.sass.dest))
       // Add browsersync stream pipe after compilation
       .pipe(browserSync.stream())
   );
-}
-
-
-//
-
-
-// Copy file from node_modules
-
-const arr = [
-  //{to: 'css', from: ['node_modules/locomotive-scroll/dist/locomotive-scroll.css']},
-  {to: 'sass/material-icons', from: ['node_modules/@mdi/font/scss/*.scss']},
-  {to: 'fonts', from: ['node_modules/@mdi/font/fonts/**']},
-  {to: 'js', from: ['node_modules/bootstrap/dist/js/bootstrap.bundle.min.js']},
-  {to: 'js', from: ['dist/index.js']},
-  {to: 'js', from: ['node_modules/swiper/swiper-bundle.min.js']},
-  {to: 'images', from: ['dist/*.jpg']},
-  {to: 'css', from: ['node_modules/swiper/swiper-bundle.min.css']}
-];
-
-function copyFiles(arr) {
-  return function() {
-    var commands = null;
-    arr.forEach(function (object) {
-      commands = commands +
-        gulp
-        .src(object.from)
-        .pipe(gulp.dest(object.to));
-    });
-    return commands;
-  }
-}
-
-//
+};
 
 // A simple task to reload the page
 function browsersyncReload(cb){
@@ -84,22 +41,31 @@ function browsersyncReload(cb){
 function watch() {
   browserSync.init({
     server: {
-      baseDir: "_ak_pages/",
+      baseDir: "./",
     }
   });
   gulp.watch("sass/**/*.scss", style);
-  gulp.watch('_ak_pages/*.html').on('change', browserSync.reload)
+  gulp.watch('./*.html').on('change', browserSync.reload)
 }
-
-// Static server
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-});
 
 exports.style = style;
 exports.watch = watch;
-exports.copy = copyFiles(arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function defaultTask(cb) {
+//   // place code for your default task here
+//   cb();
+// }
+//
+// exports.default = defaultTask
